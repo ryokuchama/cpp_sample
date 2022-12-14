2474bb5e4129cca76ca08b7e05ef5263588f9d02
ryokuchama <r.madrid.pg@gmail.com>
2022-12-14 23:07:33 +0900

    project structure

---
+++ include/crypto.h
@@ -0,0 +1,14 @@
+#ifndef CRYPTO_H_
+#define CRYPTO_H_
+
+#include <interface_crypto.h>
+
+class Crypto : public InterfaceCrypto {
+ private:
+  /* data */
+ public:
+  Crypto(/* args */) {}
+  ~Crypto() {}
+};
+
+#endif  // CRYPTO_H_
---
+++ include/interface_crypto.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_CRYPTO_H_
+#define INTERFACE_CRYPTO_H_
+
+class InterfaceCrypto {
+ public:
+  virtual ~InterfaceCrypto() {}
+};
+
+#endif  // INTERFACE_CRYPTO_H_
